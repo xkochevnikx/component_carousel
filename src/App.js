f164b7ba0887e16dbaf427c7fbab854bb3b46b66
xkochevnikx <xkochevnik@protonmail.com>
2023-06-13 09:47:02 +0300

    fix comment

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import "./App.css";
-import ImageSlider from "./components/ImageSlider/ImageSlider";
+import './App.css';
+import ImageSlider from './components/ImageSlider/ImageSlider';
 
-//? суть карусели в том что у нас есть массив у которого есть индексы. Мы создаём стейт с изначальным значением 0 и нажимая добавить например мы сначала проверяем - если текущее значение стейта равно фактической длинне массива(а это вычисляется так - массив.length - 1 ) то значит мы на крайнем элементе и мы запускаем цикл по новой перезаписывая стейт на 0, если значение меньше длинны массива то просто добавляем +1. Еще мы можем оказавшись на последнем элементе останавить функцию return-ом. А в стилях дива есть backgroundImage в значение которого динамической ссылкой подставляется массив далее индекс из стейта и этот стейт служит индексом элемента в массиве.
+//? логика карусели в том что у нас есть массив у которого есть индексы. Мы создаём сотояние с изначальным значением 0 и нажимая добавить например мы сначала проверяем - если текущее значение стейта равно фактической длинне массива(а это вычисляется так - массив.length - 1 ) то значит мы на крайнем элементе и мы запускаем цикл по новой перезаписывая сотояние на 0, если значение меньше длинны массива то просто добавляем +1. Еще мы можем оказавшись на последнем элементе останавить функцию return-ом. А в стилях дива есть backgroundImage в значение которого динамической ссылкой подставляется массив далее индекс из стейта и этот стейт служит индексом элемента в массиве.
 
 function App() {
-  const slides = [
-    { url: "https://i.ibb.co/3zWDLT0/img1.jpg", title: "ночь" },
-    { url: "https://i.ibb.co/0sLyTxX/img2.jpg", title: "пёс" },
-    { url: "https://i.ibb.co/4pPpKp9/img3.jpg", title: "жека" },
-    {
-      url: "https://ic.pics.livejournal.com/ls_online/34828672/500394/500394_original.jpg",
-      title: "арча",
-    },
-    {
-      url: "https://tuda-suda.net/wp-content/uploads/ala-archa-9.jpg",
-      title: "арча",
-    },
-  ];
+    const slides = [
+        { url: 'https://i.ibb.co/3zWDLT0/img1.jpg', title: 'ночь' },
+        { url: 'https://i.ibb.co/0sLyTxX/img2.jpg', title: 'пёс' },
+        { url: 'https://i.ibb.co/4pPpKp9/img3.jpg', title: 'жека' },
+        {
+            url: 'https://ic.pics.livejournal.com/ls_online/34828672/500394/500394_original.jpg',
+            title: 'арча',
+        },
+        {
+            url: 'https://tuda-suda.net/wp-content/uploads/ala-archa-9.jpg',
+            title: 'арча',
+        },
+    ];
 
-  const containerStyles = {
-    width: "700px",
-    height: "380px",
-    margin: "50px auto",
-  };
+    const containerStyles = {
+        width: '700px',
+        height: '380px',
+        margin: '50px auto',
+    };
 
-  return (
-    <div>
-      <div style={containerStyles}>
-        <ImageSlider slides={slides} />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div style={containerStyles}>
+                <ImageSlider slides={slides} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -1,101 +1,105 @@
-import React, { useState } from "react";
-import "./ImageSlider.css";
+import React, { useState } from 'react';
+import './ImageSlider.css';
 
 const ImageSlider = ({ slides }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-  const sliderStyles = {
-    height: "100%",
-    position: "relative",
-  };
+    const sliderStyles = {
+        height: '100%',
+        position: 'relative',
+    };
 
-  const slideStyles = {
-    width: "100%",
-    height: "100%",
-    borderRadius: "10px",
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    //? тут динамически обращаемся к массиву и объекту по индексу, далее к ключу url
-    backgroundImage: `url(${slides[currentIndex].url})`,
-  };
+    const slideStyles = {
+        width: '100%',
+        height: '100%',
+        borderRadius: '10px',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        //? тут динамически обращаемся к массиву и объекту по индексу, далее к ключу url
+        backgroundImage: `url(${slides[currentIndex].url})`,
+        transition: '0.5s',
+    };
 
-  const leftArrowStyles = {
-    position: "absolute",
-    top: "50%",
-    transform: "translate(0, -50%)",
-    left: "32px",
-    fontSize: "45px",
-    color: "#fff",
-    zIndex: 1,
-    cursor: "pointer",
-  };
+    const leftArrowStyles = {
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate(0, -50%)',
+        left: '32px',
+        fontSize: '45px',
+        color: '#fff',
+        zIndex: 1,
+        cursor: 'pointer',
+    };
 
-  const rightArrowStyles = {
-    position: "absolute",
-    top: "50%",
-    transform: "translate(0, -50%)",
-    right: "32px",
-    fontSize: "45px",
-    color: "#fff",
-    zIndex: 1,
-    cursor: "pointer",
-  };
+    const rightArrowStyles = {
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate(0, -50%)',
+        right: '32px',
+        fontSize: '45px',
+        color: '#fff',
+        zIndex: 1,
+        cursor: 'pointer',
+    };
 
-  const dotsContainerStyles = {
-    display: "flex",
-    justifyContent: "center",
-  };
+    const dotsContainerStyles = {
+        display: 'flex',
+        justifyContent: 'center',
+    };
 
-  const dotStyles = {
-    margin: "0 3px",
-    cursor: "pointer",
-    fontSize: "30px",
-  };
+    const dotStyles = {
+        margin: '0 3px',
+        cursor: 'pointer',
+        fontSize: '30px',
+    };
 
-  //? условие - если текущий стейт равен 0 то что бы уйти в последнее фото в массиве - сохраняем в переменную результат вычитания общей длинны массива минус 1 и получаем последний индекс. Его записываем в стейт и открывается последнее фото в массиве.
-  function goToPrevios() {
-    if (currentIndex === 0) {
-      let newIndex = slides.length - 1;
-      setCurrentIndex(newIndex);
-    } else {
-      setCurrentIndex(currentIndex - 1);
+    //? функция преключения на предыдущий слайд, проверяю если текущий стейт равен 0 то что бы перейти на последнее фото в массиве - сохраняем в переменную результат вычитания общей длинны массива минус 1 и получаем последний индекс. Его записываем в стейт и открывается последнее фото в массиве. Если слайд не превый то просто вычитаем 1 и переключаемся на предыдущий.
+    function goToPrevios() {
+        if (currentIndex === 0) {
+            let newIndex = slides.length - 1;
+            setCurrentIndex(newIndex);
+        } else {
+            setCurrentIndex(currentIndex - 1);
+        }
     }
-  }
 
-  //?  сравниваем стейт с длинной массива, если true то присваиваем стейту 0 индекс если нет прибавляем 1
-  function goToNext() {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  }
+    //?  функция перехода на следующий слайд, логика такая - в начале получаем индекс последнего слайда в массиве, и сравниваем - если текущий индекс равен последнему это значит что я на последнем слайде и на круг нужно заходить по новой, изменяем индекс на нулевой.
+    function goToNext() {
+        const isLastSlide = currentIndex === slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+    }
 
-  function goToSlide(slideIndex) {
-    setCurrentIndex(slideIndex);
-  }
+    //? функция перехода между слайдами по индексу, тут всё просто , перезаписываем в стейт новый индекс текщего слайда который в свою очередь автоматически подставляется в бэкграунд дива slideStyles.
+    function goToSlide(slideIndex) {
+        setCurrentIndex(slideIndex);
+    }
 
-  return (
-    <div style={sliderStyles}>
-      <div style={leftArrowStyles} onClick={goToPrevios}>
-        {" "}
-        ❮{" "}
-      </div>
-      <div style={rightArrowStyles} onClick={goToNext}>
-        {" "}
-        ❯{" "}
-      </div>
-      <div style={slideStyles}></div>
-      <div style={dotsContainerStyles}>
-        {slides.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
-            style={dotStyles}
-            onClick={() => goToSlide(slideIndex)}>
-            ⊗
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div style={sliderStyles}>
+            <div
+                style={leftArrowStyles}
+                onClick={goToPrevios}>
+                ❮
+            </div>
+            <div
+                style={rightArrowStyles}
+                onClick={goToNext}>
+                ❯
+            </div>
+            <div style={slideStyles}></div>
+            <div style={dotsContainerStyles}>
+                {slides.map((slide, slideIndex) => (
+                    <div
+                        key={slideIndex}
+                        style={dotStyles}
+                        onClick={() => goToSlide(slideIndex)}>
+                        ⊗
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default ImageSlider;
